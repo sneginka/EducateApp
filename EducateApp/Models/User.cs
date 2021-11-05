3ca2c10f59aa427170174d03b5dd883576fbb28b
Марина Геннадьевна <marina-kopina@ya.ru>
2021-11-05 05:17:41 +0500

    Создание контекста данных, написание строки подключения к любому экземпляру MS SQL Server, связь контекста и строки в файле Startup

---
+++ EducateApp/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace EducateApp.Models
+{
+    public class User : IdentityUser
+    {
+        //дополнительные поля для каждого пользователя
+        //для преподавателя могут понадобиться данные о ФИО
+
+        //сообщение об ошибке при валидации на стороне клиента
+        [Required(ErrorMessage = "Введите фамилию")]
+
+        //отображение Фамилия вместо LastName
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите отчество")]
+        [Display(Name = "Отчество")]
+        public string Patronymic { get; set; }
+
+
+        //навигационные свойства
+    }
+}
